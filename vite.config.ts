a65631d5eb8f9eedb33db9bf17b916c23adec526
HSg666 <1303574731@qq.com>
2024-01-03 15:20:41 +0800

    chore:添加sass和css自动添加浏览器兼容前缀utoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ["Chrome > 40", "ff> 31", "ie 11"],
+        }),
+      ],
+    },
+  },
   plugins: [vue()],
-})
+});
