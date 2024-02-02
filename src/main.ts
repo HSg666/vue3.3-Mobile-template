67eab841d76a0f68682adc5d6897f53cbe3f79bd
HSg666 <1303574731@qq.com>
2024-02-02 16:03:43 +0800

    fix:修复dialog丢失样式的bug

--- src/main.ts
+++ src/main.ts
@@ -19,9 +19,11 @@ import App from '@/App.vue'
 import router from './router' // 封装的路由
 import { pinia } from './store' // 封装的模块化pinia store
 import 'vant/es/toast/style'
-import { Toast } from 'vant'
+import 'vant/es/dialog/style'
+import { Toast, Dialog } from 'vant'
 
 const app = createApp(App)
 app.use(Toast)
+app.use(Dialog)
 
 app.use(pinia).use(router).mount('#app')
